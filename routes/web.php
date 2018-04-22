8f11819f83e6f8cc121ea14ee71c18377bd85fb0
Youssouf Bentaleb <b.youssouf12@gmail.com>
2018-04-22 11:16:07 +0100

    add admin controller

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', [
-    "uses" => "PublicationController@grzeerer",
+    "uses" => "PublicationController@grergerre",
     "as" => "admin.rr"]);
