9fe9633c6a1776f199d7745acc2396a4d887f1f1
BenaliDjamel <eeeee0566@gmail.com>
2018-04-23 17:19:43 +0100

    change admin  route

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin', [
-    "uses" => "PublicationController@grergerre",
+    "uses" => "PublicationController@pub",
     "as" => "admin.rr"]);
