48f5f8a2752452adae0081153499075bf64d0f59
BenaliDjamel <eeeee0566@gmail.com>
2018-05-28 04:25:39 +0100

    Set up departement , module,  formation methods

--- app/Http/Controllers/DepartementController.php
+++ app/Http/Controllers/DepartementController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Departement;
 class DepartementController extends Controller
 {
-    //
+   public function __construct() {
+       $this->middleware('admin');
+   }
+
+
+    public function store(Request $request) {
+        $dep = new Departement;
+        $dep->nom = $request->nom;
+        $dep->save();
+        return redirect()->back();
+    }
+
+    public function show_all(Request $request) {
+        $deps = Departement::all();
+        return $deps;
+    }
+    
+
+    public function edit($id,Request $request) {
+        $dep = Departement::find($id);
+        $dep->nom = $request->nom;
+        $dep->save();
+        return redirect()->back();
+    }
+
+    public function delete($id,Request $request) {
+        $dep = Departement::find($id);
+        $dep->delete();
+        return redirect()->back();
+    }
+
 }
